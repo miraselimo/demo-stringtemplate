ad0d8e8508323b61c0d279e64f75cd296ae2a376
22374604 <22374604@sun.ac.za>
2020-04-20 17:22:22 +0200

    minor changes

--- TemplateFile.java
+++ TemplateFile.java
@@ -1,6 +1,19 @@
 import org.stringtemplate.v4.*;
 
 public class TemplateFile {
-    STGroup group  = new STGroupDir("templates");
-    ST st = group.getInstanceOf("email.stg");
+    public static void main(String[] args) {
+        STGroup group  = new STGroupFile("templates/email.stg");
+        ST st = group.getInstanceOf("body");
+
+        st.add("fromaddress", "exa@example.com");
+        st.add("toaddress", "max@example.com");
+        st.add("to", "exa");
+
+        System.out.println(st.render());
+
+        ST st1 = group.getInstanceOf("signature");
+        st1.add("from", "max");
+
+        System.out.println(st1.render());
+    }
 }
