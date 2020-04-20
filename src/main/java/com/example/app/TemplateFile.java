1c58647b49a753697aa693b66759f13d9e3114c8
22374604 <22374604@sun.ac.za>
2020-04-20 22:11:41 +0200

    Add java sources

---
+++ src/main/java/com/example/app/TemplateFile.java
@@ -0,0 +1,21 @@
+package com.example.app;
+
+import org.stringtemplate.v4.*;
+
+public class TemplateFile {
+    public static void main(String[] args) {
+        STGroup group  = new STGroupFile("templates/email.stg");
+        ST st = group.getInstanceOf("body");
+
+        st.add("fromaddress", "exa@example.com");
+        st.add("toaddress", "max@example.com");
+        st.add("to", "exa");
+
+        System.out.println(st.render());
+
+        ST st1 = group.getInstanceOf("signature");
+        st1.add("from", "max");
+
+        System.out.println(st1.render());
+    }
+}
